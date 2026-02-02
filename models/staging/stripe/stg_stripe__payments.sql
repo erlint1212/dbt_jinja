c93b40a2491972acba38f6ddf3e4b1842c522663
erlint1212 <e.t.nordtvedt@gmail.com>
2026-02-02 13:30:27 +0100

    macro

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -15,7 +15,8 @@ renamed as (
         status,
 
         -- amount is stored in cents, convert it to dollars
-        amount / 100 as amount,
+        -- amount / 100 as amount,
+        {{ cents_to_dollars("amount", 4) }} as amount,
         created as created_at
 
     from source
